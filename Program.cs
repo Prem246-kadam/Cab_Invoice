50f7956ff1f3b3c261894ed14d75bdd270dedd01
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-06-04 22:52:14 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cab_Invoice_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Cab Invoice Generator Problem!");
+        }
+    }
+}
